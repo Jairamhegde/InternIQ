7563d582b9dd909d0f7441f751b747199105d6cb
jairam <jairamkansur@gmail.com>
2026-06-14 13:46:38 +0530

    Db connection updated

--- dbconnection/dbconnect.py
+++ dbconnection/dbconnect.py
@@ -1,28 +1,55 @@
 import psycopg2
 import streamlit as st
+import os
+import logging
 
 
-@st.cache_resource
+
 def connect_database(search_path="clean_data"):
-    data = st.secrets["database"]
-    PG_CONFIG = {
-        "host": data['host'],
-        "database": data['database'],
-        "user": data['user'],
-        "password": data['password'],
-        "port": data['port'],
-        "sslmode" : data['sslmode']
-    }
-    try:
-        conn = psycopg2.connect(**PG_CONFIG)
-        cur = conn.cursor()
-        cur.execute("SET search_path TO %s", (search_path,))
-        cur.close()
-      
-        return conn
-    except psycopg2.OperationalError as e:
-        raise ConnectionError(
-            f"Could not connect to PostgreSQL database '{PG_CONFIG['database']}' "
-            f"at {PG_CONFIG['host']}:{PG_CONFIG['port']}. Check your config and ensure the server is running.\n{e}"
-        )
-    
+
+    if os.environ.get("HOST_NAME"):
+        PG_CONFIG = {
+            "host": os.environ['HOST_NAME'],
+            "database": os.environ['DATABASE'],
+            "user": os.environ['USER'],
+            "password": os.environ['PASSWORD'],
+            "port": os.environ['PORT'],
+            "sslmode" : os.environ['SSLMODE']
+        }
+        try:
+            conn = psycopg2.connect(**PG_CONFIG)
+            cur = conn.cursor()
+            cur.execute("SET search_path TO %s",(search_path,))
+            cur.close()
+            return conn
+        except Exception as e:
+            logging.error(f"Error occured in db_connection :{e}")
+            
+    else:
+        return  _connect_to_st(search_path)
+         
+@st.cache_resource
+def _connect_to_st(search_path):
+        data = st.secrets["database"]
+        PG_CONFIG = {
+            "host": data['host'],
+            "database": data['database'],
+            "user": data['user'],
+            "password": data['password'],
+            "port": data['port'],
+            "sslmode" : data['sslmode']
+        }
+        try:
+            conn = psycopg2.connect(**PG_CONFIG)
+            cur = conn.cursor()
+            cur.execute("SET search_path TO %s", (search_path,))
+            cur.close()
+        
+            return conn
+        except psycopg2.OperationalError as e:
+            raise ConnectionError(
+                f"Could not connect to PostgreSQL database '{PG_CONFIG['database']}' "
+                f"at {PG_CONFIG['host']}:{PG_CONFIG['port']}. Check your config and ensure the server is running.\n{e}"
+            )
+        
+

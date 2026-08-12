c2e2e8a4ee564d42d7f5a69818887253aa47e13d
jairam <jairamkansur@gmail.com>
2026-08-12 13:20:59 +0530

    feat: add isolated raw data ingestion script for daily scraping pipeline

---
+++ store_rawdata.py
@@ -0,0 +1,66 @@
+from extract.fetcher import get_soup
+from extract.extractor import scrape_data
+from db.db_manager import manage_operation
+
+
+soup = get_soup()
+
+import logging
+import psycopg2
+from extract.fetcher import get_soup
+from extract.extractor import scrape_data
+
+from rawData.raw_data import insertRawData
+from db.db_manager import manage_operation
+from transform.transformData import loadData
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=(
+        "%(asctime)s | %(levelname)s | "
+        "%(filename)s:%(lineno)d | "
+        "%(funcName)s() | %(message)s"
+    ),
+    handlers=[
+        logging.FileHandler("logfile.log"),
+        logging.StreamHandler()
+    ]
+)
+
+page_url = [
+    "https://internshala.com/jobs/machine-learning-jobs/",
+    "https://internshala.com/jobs/backend-development-jobs/",
+    "https://internshala.com/fresher-jobs/front-end-development-jobs/",
+    "https://internshala.com/jobs/mobile-app-development-jobs/",
+    "https://internshala.com/jobs/big-data-jobs/"
+]
+
+
+def internshala(url_list):
+    logging.info("Execution started...")
+
+    for url in url_list:
+        for page in range(1, 5):
+
+            link = url if page == 1 else f"{url}page-{page}"
+            try:
+                soup     = get_soup(link)
+                raw_data = scrape_data(soup)
+
+                if not raw_data:
+                    logging.info(f"No data found on page {page} — stopping pagination for {url}")
+                    break
+
+                logging.info(f"Scraped {len(raw_data)} jobs from {link}")
+
+                insertRawData(raw_data)
+
+                logging.info(f"Inserted {len(raw_data)} jobs into raw_data schema (PostgreSQL)")
+
+            except Exception:
+                logging.exception(f"Failed scraping page {page} of {url}")
+
+
+if __name__ == '__main__':
+    internshala(url_list=page_url)

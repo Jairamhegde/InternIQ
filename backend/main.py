a1805559d9504de1d327368cfaecd7a52b318a84
jairam <jairamkansur@gmail.com>
2026-08-16 23:44:17 +0530

    updated main.py for CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -52,10 +52,11 @@ class JobpostingModel(BaseModel):
 
 
 # Allow all origins for local development
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
